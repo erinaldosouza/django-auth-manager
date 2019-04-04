e8ad4007993c46dc8a7a03e30ec6acf2ba729793
Francisco Erinaldo de Souza <t_francisco@hapvida.com.br>
2019-04-04 17:35:10 -0300

    removing venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\t_francisco\PycharmProjects\DjangoAuthManager\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
