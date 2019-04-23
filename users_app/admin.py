d5ef818ece6152b19b1fb8161723d84188aa07ad
Francisco Erinaldo de Souza <t_francisco@hapvida.com.br>
2019-04-23 11:01:56 -0300

    user app admin

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import UserApp
 
+
+class UserAppAdmin(admin.ModelAdmin):
+    list_display = ('login', 'name', 'last_name')
+    list_filter = ('login', 'name', 'last_name')
+    search_fields = ('login', 'name', 'last_name')
+
+
 # Register your models here.
-admin.site.register(UserApp)
+admin.site.register(UserApp, UserAppAdmin)
