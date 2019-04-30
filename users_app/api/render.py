b1c88f5258062d69b1898a72efc1cda132f7ce09
Francisco Erinaldo de Souza <t_francisco@hapvida.com.br>
2019-04-30 13:20:26 -0300

    http code 204 when no user found

--- users_app/api/render.py
+++ users_app/api/render.py
@@ -1,9 +1,12 @@
-from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer, BaseRenderer
+from rest_framework.renderers import JSONRenderer, BaseRenderer
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 
 class UserAppJsonRenderer(JSONRenderer, BaseRenderer):
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
+
+        print(renderer_context['response'].status_code)
 
         if len(data) == 1:
             data = data[0:1][0]
@@ -14,5 +17,6 @@ class UserAppJsonRenderer(JSONRenderer, BaseRenderer):
 
         else:
             data = None
+            renderer_context['response'].status_code = HTTP_204_NO_CONTENT
 
         return super(UserAppJsonRenderer, self).render(data, accepted_media_type, renderer_context)
